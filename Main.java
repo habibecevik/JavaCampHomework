551bba875ec3ee7a9df5cf7f9ac9222d13e23254
habibecevik1 <>
2021-05-13 16:33:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    Product product1 = new Product(1, "C# + Angular" , "Engin Demirog");
+		Product product2 = new Product(2, "Java + REACT" , "Engin Demirog");
+    Product product3 = new Product(3, "Proglamaya Giriş İçin Temel Kurs", "Engin Demirog");
+
+
+
+    Product[] products = {product1,product2,product3};
+		
+		for (Product product : products) {
+			
+			System.out.println(product.name + product.teacher);
+
+  }
+  ProductManager productManager = new ProductManager();
+		productManager.addToCart(product1);
+		productManager.addToCart(product2);
+    productManager.addToCart(product3);
+
+}
+}
---
+++ product.java
@@ -0,0 +1,14 @@
+public class Product {
+
+	
+	public Product(int id,String name,String teacher ) { 
+		this.id=id;
+		this.name=name;
+		this.teacher=teacher;
+		
+	}
+	
+	int id;
+	String name;
+	String teacher;
+}
