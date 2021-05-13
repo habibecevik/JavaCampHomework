d9b4c485252a57c31205d5094c15ea6e797dd82e
habibecevik <77547723+habibecevik@users.noreply.github.com>
2021-05-13 19:46:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,23 +2,27 @@ class Main {
 
   public static void main(String[] args) {
     
-    Product product1 = new Product(1, "C# + Angular" , "Engin Demirog");
-		Product product2 = new Product(2, "Java + REACT" , "Engin Demirog");
-    Product product3 = new Product(3, "Proglamaya Giriş İçin Temel Kurs", "Engin Demirog");
+          Product product1 = new Product(1, "C# + Angular" , "Engin Demirog");
+          Product product2 = new Product(2, "Java + REACT" , "Engin Demirog");
+          Product product3 = new Product(3, "Proglamaya Giriş İçin Temel Kurs", "Engin Demirog");
 
 
 
-    Product[] products = {product1,product2,product3};
+          Product[] products = {product1,product2,product3};
 		
-		for (Product product : products) {
-			
+	  for (Product product : products) {
+		  
 			System.out.println(product.name + product.teacher);
 
-  }
-  ProductManager productManager = new ProductManager();
-		productManager.addToCart(product1);
-		productManager.addToCart(product2);
-    productManager.addToCart(product3);
+          }
+          ProductManager productManager = new ProductManager();
+	  productManager.addToCart(product1);
+	  productManager.addToCart(product2);
+          productManager.addToCart(product3);
 
-}
-}
+
+      }
+	
+}	
+	
+	
